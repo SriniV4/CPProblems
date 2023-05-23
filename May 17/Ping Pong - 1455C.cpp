f84fa004a13dcc285c525cd2951ff32a8eef1e80
SriniV <89159939+SriniV4@users.noreply.github.com>
2023-05-22 21:06:29 -0500

    Create Ping Pong - 1455C.cpp
    https://codeforces.com/contest/1455/problem/C

---
+++ May 17/Ping Pong - 1455C.cpp
@@ -0,0 +1,28 @@
+//https://codeforces.com/contest/1455/problem/C
+#include <bits/stdc++.h>
+
+typedef long long ll;
+typedef long double ld;
+using namespace std;
+void setIO(){
+	cin.tie(0) -> sync_with_stdio(0);
+}
+
+void solve(){
+	int a , b;
+	cin >> a >> b;
+	cout << a-1 << " " << b << "\n";
+}
+
+int main(){
+	setIO();
+	int t = 1;
+	cin >> t;
+	while(t--)solve();
+}
+
+
+
+
+
+
