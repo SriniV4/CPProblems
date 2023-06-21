b01a3dca536a1049a18f83c99c08f6cbfe2863c5
SriniV <89159939+SriniV4@users.noreply.github.com>
2023-06-21 07:37:21 -0500

    Create Lucky String.cpp

---
+++ June 12/Lucky String.cpp
@@ -0,0 +1,57 @@
+/*
+	Date: 
+	Time:
+	Learnt:
+	Rating:
+*/
+#include <bits/stdc++.h>
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+#define PI 3.14159265358979323846
+#define sbits(x) __builtin_popcount(x)
+#define tbits(total_size , num) total_size - __builtin_clz(num)
+#define pb push_back
+#define trav(name , ds) for(auto&name : ds)
+#define f first
+#define s second
+#define clr(ds) ds.clear()
+#define all(ds) ds.begin() , ds.end()
+#define pi pair<int , int>
+#define vi vector<int>
+#define vll vector<ll>
+#define vpi vector<pi>
+#define sz(i) i.size()
+#define rsz(ds ,size , val) ds.assign(size , val);
+int xP[] = {0,0,1,-1,1,1,-1,-1} , yP[] = {1,-1,0,0,1,-1,-1,1};
+using namespace std;
+
+size_t hF(pair<int , int> a){
+	return (a.first + a.second)*(a.first + a.second + 1)/2 + a.second;
+}
+void setIO(string name = "") {
+	cin.tie(0)->sync_with_stdio(0);
+	if (name.size()) {
+		freopen((name + ".in").c_str(), "r", stdin);
+		freopen((name + ".out").c_str(), "w", stdout);
+	}
+}
+
+void solve(){
+	int n;
+	string total = "abcd";
+	cin >> n;
+	for(int i = 0;i<n;i++){
+			cout << total[i%4] ;
+	}
+}
+
+int main(){
+	setIO();
+	int t = 1;
+// 	cin >> t;
+	while(t--){
+		solve();
+	}	
+}
+
