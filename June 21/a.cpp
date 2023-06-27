ac654926e78ea31f17cb19996f20d6a376e63519
SriniV <89159939+SriniV4@users.noreply.github.com>
2023-06-27 08:24:42 -0500

    Add files via upload

---
+++ June 21/a.cpp
@@ -0,0 +1,81 @@
+	/*
+		Date: 
+		Time:
+		Learnt:
+		Rating:
+	*/
+	#include <bits/stdc++.h>
+	typedef long long ll;
+	typedef long double ld;
+	typedef unsigned long long ull;
+	#define PI 3.14159265358979323846
+	#define sbits(x) __builtin_popcount(x)
+	#define tbits(total_size , num) total_size - __builtin_clz(num)
+	#define pb push_back
+	#define trav(name , ds) for(auto&name : ds)
+	#define f first
+	#define s second
+	#define clr(ds) ds.clear()
+	#define all(ds) ds.begin() , ds.end()
+	#define pii pair<int , int>
+	#define vi vector<int>
+	#define vll vector<ll>
+	#define vpi vector<pi>
+	#define sz(i) (int)i.size()
+	#define rsz(ds ,size , val) ds.assign(size , val);
+	int xP[] = {0,0,1,-1,1,1,-1,-1} , yP[] = {1,-1,0,0,1,-1,-1,1};
+	using namespace std;
+	uint64_t time() {
+	  using namespace std::chrono;
+	  return duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
+	}
+	size_t hF(pair<int , int> a){
+		return (a.first + a.second)*(a.first + a.second + 1)/2 + a.second;
+	}
+	void setIO(string name = "") {
+		cin.tie(0)->sync_with_stdio(0);
+		if (name.size()) {
+			freopen((name + ".in").c_str(), "r", stdin);
+			freopen((name + ".out").c_str(), "w", stdout);
+		}
+	}
+
+	void solve(){
+		int n;
+		cin >> n;
+		vi arr(n);
+		ll ans = 0;
+		trav(a , arr){
+			cin >> a;
+			ans+=abs(a);
+		}
+		cout << ans << " ";
+		int moves = 0;
+		int counter = 0;
+		for(int i =0;i<n;i++){
+					if(arr[i]<0){
+							counter++;
+					} else {
+						if(counter&&arr[i]==0)
+							continue;
+							if(counter)
+							{
+								moves++;
+								counter = 0;
+							}	
+					}
+		}
+		if(counter)
+			moves++;
+		cout << moves << "\n";
+	}
+
+	int main(){
+		setIO();
+		int t = 1;
+		cin >> t;
+		while(t--){
+			solve();
+		}	
+	}
+
